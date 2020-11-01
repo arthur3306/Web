e136b4d8aee495c96ebb1a59b151f1da8e460cb4
arthur3306 <57452965+arthur3306@users.noreply.github.com>
2020-11-01 11:44:21 -0300

    Add files via upload

---
+++ starts-cursos/src/pages/Course03/scripts/render.js
@@ -0,0 +1,28 @@
+
+const categoria01 = data[2];
+
+function getYouTubeId(youtubeURL) {
+    return youtubeURL
+      .replace(
+        /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/,
+        '$7',
+      );
+  }
+  
+
+const video = document.getElementById('videos');
+ 
+const content = categoria01.videos.map((video) => {
+    const videoId = getYouTubeId(video.url);
+
+        return `
+            <li>
+            <h3>${video.titulo} <br>⇓</h3>
+            <iframe width="900" height="400" class="video" src="https://www.youtube.com/embed/${videoId}" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>> </iframe>
+            </li>
+        `;
+    
+}).join(' ')
+
+video.innerHTML = content;
+
